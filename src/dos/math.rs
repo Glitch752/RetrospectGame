09444de08e41529a373bf87a9777ff496acd6658
Glitch752 <xxGlitch752xx@gmail.com>
2024-09-26 20:22:25 -0500

    Reimplement donut with faster fixed-point math

--- src/dos/math.rs
+++ src/dos/math.rs
@@ -1,15 +1,5 @@
-use core::intrinsics;
-
 const fn num_bits<T>() -> usize { core::mem::size_of::<T>() * 8 }
 
 pub const fn log_2(x: usize) -> usize {
     num_bits::<usize>() as usize - x.leading_zeros() as usize - 1
-}
-
-pub fn sin(theta: f32) -> f32 {
-    unsafe { intrinsics::sinf32(theta) }
-}
-
-pub fn cos(theta: f32) -> f32 {
-    unsafe { intrinsics::cosf32(theta) }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,76 @@
 #![no_std]
 #![no_main]
-#![feature(core_intrinsics)]
+// #![feature(core_intrinsics)]
 
 extern crate alloc;
-use core::intrinsics;
 
 use rust_dos::*;
 
 entry!(main);
 
+fn rotate(mul: i32, shift: i32, x: &mut i32, y: &mut i32) {
+    let mut old_x = *x;
+    *x -= mul * *y >> shift;
+    *y += mul * old_x >> shift;
+    old_x = 3145728 - *x * *x - *y * *y >> 11;
+    *x = *x * old_x >> 10;
+    *y = *y * old_x >> 10;
+}
+
 fn main() {
-    println!("Hello, world!");
-    
-    // Donut.c ported to Rust
-    let mut a: f32 = 0.0;
-    let mut b: f32 = 0.0;
+    let mut sin_a: i32 = 1024;
+    let mut cos_a: i32 = 0;
+    let mut sin_b: i32 = 1024;
+    let mut cos_b: i32 = 0;
     
     loop {
-        a += 0.07;
-        b += 0.03;
-
-        let sin_a = dos::math::sin(a);
-        let cos_a = dos::math::cos(a);
-        let sin_b = dos::math::sin(b);
-        let cos_b = dos::math::cos(b);
-
-        let mut z = [0.0; 1760];
+        let mut z = [i8::MAX; 1760];
         let mut buf: [char; 1760] = [' '; 1760];
 
-        for j in (79..1760).step_by(80) {
-            buf[j] = '\n';
-        }
+        let mut sj = 0;
+        let mut cj = 1024;
+        for _ in 0..90 {
+            let mut si = 0;
+            let mut ci = 1024;
 
-        let mut j: f32 = 0.0;
-        while j < 6.28 {
-            let j_sin = dos::math::sin(j);
-            let j_cos = dos::math::cos(j);
+            for _ in 0..324 {
+                let r1 = 1;
+                let r2 = 2048;
+                let k2 = 5120 * 1024;
+        
+                let x0 = r1 * cj + r2;
+                let x1 = ci * x0 >> 10;
+                let x2 = cos_a * sj >> 10;
+                let x3 = si * x0 >> 10;
+                let x4 = r1 * x2 - (sin_a * x3 >> 10);
+                let x5 = sin_a * sj >> 10;
+                let x6 = k2 + r1 * 1024 * x5 + cos_a * x3;
+                let x7 = cj * si >> 10;
+                let x = 40 + 30 * (cos_b * x1 - sin_b * x4) / x6;
+                let y = 12 + 15 * (cos_b * x4 + sin_b * x1) / x6;
+                let n = (-cos_a * x7 - cos_b * ((-sin_a * x7 >> 10) + x2) - ci * (cj * sin_b >> 10) >> 10) - x5 >> 7;
 
-            let mut i: f32 = 0.0;
-            while i < 6.28 {
-                let i_sin = dos::math::sin(i);
-                let i_cos = dos::math::cos(i);
-
-                let h = j_cos + 2.0;
-                let d = 1.0 / (i_sin * h * sin_a + j_sin * cos_a + 5.0);
-                let t = i_sin * h * cos_a - j_sin * sin_a;
-
-                let x = unsafe { intrinsics::floorf32(40.0 + 30.0 * d * (i_cos * h * cos_b - t * sin_b)) };
-                let y = unsafe { intrinsics::floorf32(12.0 + 15.0 * d * (i_cos * h * sin_b + t * cos_b)) };
-                let o = x + 80.0 * y;
-                
-                if y >= 0.0 && y < 22.0 && x >= 0.0 && x < 79.0 {
-                    if d > z[o as usize] {
-                        z[o as usize] = d;
-                        const CHARS: [char; 12] = ['.', ',', '-', '~', ':', ';', '=', '!', '*', '#', '$', '@'];
-                        buf[o as usize] = CHARS[unsafe { intrinsics::floorf32(
-                            intrinsics::maxnumf32(0.0, 8.0 * ((j_sin * sin_a - i_sin * j_cos * cos_a) * cos_b - i_sin * j_cos * sin_a - j_sin * cos_a - i_cos * j_cos * sin_b))
-                        ) } as usize];
-                    }
+                let o = (x + 80 * y) as usize;
+                let zz: i8 = ((x6 - k2) >> 15) as i8;
+                if y < 22 && y > 0 && x > 0 && x < 80 && zz < z[o] {
+                    z[o] = zz;
+                    buf[o] = ['.', ',', '-', '~', ':', ';', '=', '!', '*', '#', '$', '@'][(if n > 0 { n } else { 0 }) as usize];
                 }
-
-                i += 0.02;
+                rotate(5, 8, &mut ci, &mut si);
             }
-            j += 0.07;
+            rotate(9, 7, &mut cj, &mut sj);
         }
-
+ 
+        print!("\x1B[H");
         for k in 0..1760 {
-            if buf[k] == '\n' {
+            if k % 80 == 0 {
                 print!("\n\r");
             } else {
                 print!("{}", buf[k]);
             }
         }
 
-        a += 0.04;
-        b += 0.02;
+        rotate(5, 7, &mut cos_a, &mut sin_a);
+        rotate(5, 8, &mut cos_b, &mut sin_b);
     }
 }
